940c3a513607a6c0ed3778ac65c619c1bd9d1c1d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-09-03 13:22:58 -0300

    feat: novo tamanho

--- src/components/mensagem-externa-table.tsx
+++ src/components/mensagem-externa-table.tsx
@@ -171,7 +171,7 @@ export function MensagemExternaTable({
                       />
                       <Button
                         variant="ghost"
-                        size="icon-lg"
+                        size="lg"
                         onClick={(event) => {
                           event.stopPropagation();
                           deleteMessage(message.id);
